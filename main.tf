28589aa14692484f35129b77f8bf20a6b18cdc18
PremkumarManoharan <raghavamanoharan.p@northeastern.edu>
2023-09-17 15:35:56 -0400

    used terraform input and output variables

--- main.tf
+++ main.tf
@@ -1,6 +1,11 @@
 provider "aws" {
   region = "us-east-2"
 }
+variable "server_port" {
+  description = "The port the server will use for HTTP request"
+  type = number
+  default = 8080
+}
 
 resource "aws_instance" "example" {
 
@@ -11,7 +16,7 @@ resource "aws_instance" "example" {
     user_data = <<-EOF
                 #!/bin/bash
                 echo "Hello Web server" > index.html
-                nohup busybox httpd -f -p 8080 &
+                nohup busybox httpd -f -p ${var.server_port} &
                 EOF
     user_data_replace_on_change = true
 
@@ -20,12 +25,16 @@ resource "aws_instance" "example" {
     }
 }
 
+output "public_ip" {
+  value = aws_instance.example.public_ip
+  description = "The public ip address of the web server"
+}
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = var.server_port
+    to_port     = var.server_port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
